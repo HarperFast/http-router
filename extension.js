89744c7359148d2fd97ec8aebcb3df4f85bf2486
Kris Zyp <kriszyp@gmail.com>
2024-12-02 12:24:02 -0700

    Request to router

--- extension.js
+++ extension.js
@@ -4,6 +4,7 @@ export function start(options = {}) {
 			const routes = await import(file_path);
 			const servers = options.server.http(async (request, nextHandler) => {
 				// handle the routing. nextHandler can be called to delegate to the main origin server
+				return routes.onRequest(request);
 			});
 		}
 	}

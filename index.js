bf9b256efa003b06fe228a7c6f9cb68f776eade6
Kris Zyp <kriszyp@gmail.com>
2024-12-02 12:22:49 -0700

    Add some more structure

--- index.js
+++ index.js
@@ -2,7 +2,24 @@
  * The main router class for defining a set of routes and their handlers.
  */
 export class Router {
+	rules = [];
 	get(path, handler) {
+		this.rules.push({ path, method: 'GET', handler });
 		return this;
+	}
+
+	/**
+	 * For each incoming request, perform routing based on the defined rules
+	 * @param request
+	 */
+	onRequest(request) {
+
+	}
+}
+
+export function or(...conditions) {
+	return {
+		conditions,
+		operator: 'or',
 	}
 }

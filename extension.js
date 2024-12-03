295b73bf186833d3aba5a8da2a621ffc8067e24f
Kris Zyp <kriszyp@gmail.com>
2024-12-02 18:28:14 -0700

    Directly use the caching handler

--- extension.js
+++ extension.js
@@ -1,10 +1,17 @@
+import { getCacheHandler } from '@harperdb/http-cache/extension.js';
 export function start(options = {}) {
+	let cacheHandler = getCacheHandler(options);
 	return {
 		async handleFile(js, url_path, file_path) {
 			const routes = await import(file_path);
 			const servers = options.server.http(async (request, nextHandler) => {
-				// handle the routing. nextHandler can be called to delegate to the main origin server
-				return routes.onRequest(request);
+				// set the cache key for the request, applying any rules for the cache key
+				request.cacheKey = routes.getCacheKey(request);
+				// let the cache attempt to resolve
+				return cacheHandler(request, () => {
+					// wasn't cached, handle the routing. nextHandler can be called to delegate to the main origin server
+					return routes.onRequest(request, nextHandler);
+				});
 			});
 		}
 	}
--- index.js
+++ index.js
@@ -7,12 +7,33 @@ export class Router {
 		this.rules.push({ path, method: 'GET', handler });
 		return this;
 	}
+	use() {
+		// I think this was just for setting the default route
+		return this;
+	}
+
+	/**
+	 * Determine the cache key for a request based on any defined rules that define it
+	 * @param request
+	 * @return {*|string}
+	 */
+	getCacheKey(request) {
+		for (let rule of this.rules) {
+			if (rule.match(request)) {
+				if (rule.getCacheKey) {
+					return rule.getCacheKey(request);
+				}
+			}
+		}
+		return request.url;
+	}
+
 
 	/**
 	 * For each incoming request, perform routing based on the defined rules
 	 * @param request
 	 */
-	onRequest(request) {
+	onRequest(request, nextHandler) {
 
 	}
 }

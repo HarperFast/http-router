c7eaa1c484ebd1592f3774961a081735130c0ee4
Kris Zyp <kriszyp@gmail.com>
2024-12-03 08:40:32 -0700

    Better rule matching logic

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 export class Router {
 	rules = [];
 	get(path, options) {
-		this.rules.push(new Rule({ path, method: 'GET'}));
+		this.rules.push(new Rule({ path, method: 'GET' }));
 		return this;
 	}
 	use() {
@@ -39,7 +39,6 @@ export class Router {
 		return this;
 	}
 
-
 	/**
 	 * For each incoming request, perform routing based on the defined rules
 	 * @param request
@@ -60,8 +59,18 @@ export class Router {
 	}
 }
 class Rule {
+	condition = {};
 	constructor(condition, options) {
-		this.condition = condition;
+		if (typeof condition === 'string') {
+			this.condition.path = stringToRegex(condition);
+		} else if ((typeof condition) instanceof RegExp) {
+			this.condition.path = condition;
+		} else {
+			if (condition.path) {
+				this.condition.path = stringToRegex(condition.path);
+			}
+			if (condition.query) this.condition.query = condition.query;
+		}
 		if (typeof options === 'function') {
 			this.handler = options;
 		} else {
@@ -70,7 +79,7 @@ class Rule {
 	}
 	match(request) {
 		if (this.condition.path) {
-			if (this.condition.path === request.url) {
+			if (this.condition.path.test(request.url)) {
 				return true;
 			}
 		}
@@ -90,39 +99,76 @@ class RequestActions {
 	constructor(request) {
 		this.request = request;
 	}
-	setResponseHeader(key, value) {
-		if (!this.responseHeaders) this.responseHeaders = new Map();
-		this.responseHeaders.set(key, value);
+	// we do theses as a getters, because the function is accessed through destructuring and called without its
+	// context/this
+	get setResponseHeader() {
+		// This should also work with middleware that returns a response object, but that's not how the
+		// next.js middleware works
+		const nodeResponse = this.request._nodeResponse;
+		return (key, value) => {
+			nodeResponse.setHeader(key, value);
+		};
 	}
-	cache(options) {
-		let client = options.browser ?? options.edge;
-		if (client?.maxAgeSeconds) {
-			this.maxAgeSeconds = client.maxAgeSeconds;
-		}
+	get cache() {
+		let actions = this;
+		return (options) => {
+			let client = options.browser ?? options.edge;
+			if (client?.maxAgeSeconds) {
+				actions.maxAgeSeconds = client.maxAgeSeconds;
+			}
+		};
 	}
 	async run(handler) {
 		if (this.redirect) {
 			return {
 				status: this.redirect.status,
 				headers: { Location: this.redirect.location },
-			}
+			};
 		}
 		let response = await handler(this.request);
-		if (this.responseHeaders) {
-			for (let [key, value] of this.responseHeaders) {
-				response.headers.set(key, value);
+		if (response) {
+			if (this.responseHeaders) {
+				for (let [key, value] of this.responseHeaders) {
+					response.headers.set(key, value);
+				}
 			}
-		}
-		if (this.maxAgeSeconds) {
-			response.headers.set('Cache-Control', `max-age=${this.maxAgeSeconds}`);
+			if (this.maxAgeSeconds) {
+				response.headers.set('Cache-Control', `max-age=${this.maxAgeSeconds}`);
+			}
 		}
 		return response;
 	}
 }
 
 export function or(...conditions) {
-	return {
-		conditions,
-		operator: 'or',
+	return new OrRule(conditions);
+}
+class OrRule {
+	constructor(conditions) {
+		this.conditions = conditions;
 	}
+	match(request) {
+		for (let condition of this.conditions) {
+			if (condition.match(request)) {
+				return true;
+			}
+		}
+	}
+}
+
+function stringToRegex(str) {
+	if (str instanceof RegExp) return str;
+	if (typeof str === 'string') {
+		return new RegExp(
+			'^' +
+				str.replace(/:[^/]+\*?/g, (match) => {
+					if (match.endsWith('*')) {
+						return '(.*)';
+					} else {
+						return '([^/]+)';
+					}
+				}) +
+				'$'
+		);
+	} else throw new TypeError('Unknown type of matching requests ' + str);
 }
